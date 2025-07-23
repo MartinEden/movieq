461babb66f804b925200e8b53838e23e0ad5c274
Martin Eden <martin.s.eden@gmail.com>
2025-07-23 20:59:16 +0100

    Cache thumbnails locally

--- src/main/kotlin/MovieQApp.kt
+++ src/main/kotlin/MovieQApp.kt
@@ -1,7 +1,5 @@
 package eden.movieq
 
-import eden.movieq.models.ImageInfo
-import eden.movieq.models.Movie
 import eden.movieq.services.ImdbService
 import eden.movieq.services.StorageService
 import gg.jte.ContentType
@@ -12,7 +10,6 @@ import io.javalin.http.Context
 import io.javalin.http.staticfiles.Location
 import io.javalin.rendering.template.JavalinJte
 import kotlinx.serialization.json.Json
-import java.time.LocalDate
 
 class MovieQApp(val imdbService: ImdbService, val store: StorageService) {
     private val serializer = Json { prettyPrint = true }
@@ -46,19 +43,8 @@ class MovieQApp(val imdbService: ImdbService, val store: StorageService) {
     fun saveHandler(ctx: Context) {
         val id = ctx.formParam("movieId") ?: throw Exception("No movieId was provided")
         val reason = ctx.formParam("reason") ?: throw Exception("No reason was provided")
-        val title = imdbService.get(id) ?: throw Exception("Couldn't find title $id in IMDB service")
-        store.save(Movie(
-            imdbId = id,
-            reason = reason,
-            title = title.primaryTitle,
-            synopsis = "MISSING", // TODO: Synopsis
-            year = title.startYear,
-            dateAdded = LocalDate.now(),
-            rating = title.rating?.aggregateRating?.times(10)?.toInt(),
-            tomatoMeter = null, // TODO: Tomato Meter
-            thumbnail = title.primaryImage?.url ?: ImageInfo.default.url, // TODO: Cache lower resolution version locally
-            tags = listOf("tag1", "tag2"), // TODO: Tags
-        ))
+        val movie = imdbService.get(id, reason)
+        store.save(movie)
         ctx.redirect("/")
     }
 
@@ -72,5 +58,10 @@ class MovieQApp(val imdbService: ImdbService, val store: StorageService) {
             staticFiles.location = Location.CLASSPATH
             staticFiles.mimeTypes.add(io.javalin.http.ContentType.JAVASCRIPT, "static/js")
         }
+    }
+
+    companion object {
+        val thumbnailPath: String = MovieQApp::class.java.classLoader.getResource("static/thumbnails")?.file
+            ?: throw Exception("Unable to get thumbnail directory")
     }
 }
--- src/main/kotlin/services/ImdbService.kt
+++ src/main/kotlin/services/ImdbService.kt
@@ -1,6 +1,9 @@
 package eden.movieq.services
 
+import eden.movieq.MovieQApp
 import eden.movieq.models.FullTitleInfo
+import eden.movieq.models.ImageInfo
+import eden.movieq.models.Movie
 import eden.movieq.models.SearchResult
 import eden.movieq.models.TitleInfo
 import io.ktor.client.*
@@ -8,8 +11,13 @@ import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.bodyAsChannel
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.util.cio.writeChannel
+import io.ktor.utils.io.copyAndClose
 import kotlinx.coroutines.runBlocking
+import java.io.File
+import java.time.LocalDate
 
 class ImdbService(val endpointURL: String) {
     private val client: HttpClient = HttpClient(CIO) {
@@ -29,9 +37,34 @@ class ImdbService(val endpointURL: String) {
         return result.titles
     }
 
-    fun get(movieId: String): FullTitleInfo? {
-        return runBlocking {
-            client.get("$endpointURL/titles/$movieId").body()
+    private fun downloadThumbnailAndGetPath(movieId: String, info: ImageInfo?): String {
+        return if (info != null) {
+            runBlocking {
+                val file = File(MovieQApp.thumbnailPath, movieId)
+                client.get(info.url).bodyAsChannel().copyAndClose(file.writeChannel())
+                "/static/thumbnails/$movieId"
+            }
+        } else {
+            ImageInfo.default.url
         }
+    }
+
+    fun get(movieId: String, reason: String): Movie {
+        val title: FullTitleInfo = runBlocking {
+            client.get("$endpointURL/titles/$movieId").body()
+                ?: throw Exception("Couldn't find title $movieId in IMDB service")
+        }
+        return Movie(
+            imdbId = movieId,
+            reason = reason,
+            title = title.primaryTitle,
+            synopsis = "MISSING", // TODO: Synopsis
+            year = title.startYear,
+            dateAdded = LocalDate.now(),
+            rating = title.rating?.aggregateRating?.times(10)?.toInt(),
+            tomatoMeter = null, // TODO: Tomato Meter
+            thumbnail = downloadThumbnailAndGetPath(movieId, title.primaryImage),
+            tags = listOf("tag1", "tag2"), // TODO: Tags
+        )
     }
 }
